d04856d5ea3d2c9d2edd27ecbeec16f002ee3293
silentmovie <37518091+silentmovie@users.noreply.github.com>
2025-03-28 22:22:59 -0700

    add the Twocycle graph related

---
+++ Solver/ID_TwoCycle.m
@@ -0,0 +1,33 @@
+function [pai, Qrow, minEig] = ID_TwoCycle(seed,N)
+
+% rng(seed,"twister")
+% pai = rand(1,N);                   
+% pai = pai/sum(pai);
+% degree = 2*ones(1,N);
+% degree(floor(N/3))= 3;
+% degree(N-floor(N/3))=3;
+
+% manual add target distribution pai and degree matrix
+pai = [4/27 4/27 4/27 1/18 1/18 4/27 4/27 4/27];
+
+degree = [2 2 3 2 2 3 2 2];
+
+Q = AcceptReject_RW(pai,degree);
+edge = edgetTwoCycle_RW(degree);
+Q = Q.*edge;
+Qrow = RowSumZero(Q)
+
+Eigenvalue = eig(Qrow);
+minEig = max(Eigenvalue(abs(Eigenvalue)>=1e-3));
+
+% check if detailed balance:
+DB = diag(pai)*Qrow;
+SymDiff = abs(DB- DB');
+if any(SymDiff >=1e-7,"all")
+    warning('not detailed balance')
+    pause
+end
+
+clear DB SymDiff
+
+end
