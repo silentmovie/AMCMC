0540db0b686c96cb33e76056f800be1331ed7c67
silentmovie <37518091+silentmovie@users.noreply.github.com>
2026-01-07 11:02:22 -0800

    change plot setting in plotLattice

--- TestCases/plotLattice.m
+++ TestCases/plotLattice.m
@@ -3,16 +3,18 @@ function out=plotLattice(rho)
 
 
 % select frames to plot
-rows_to_plot(1:4) = [1, 1000, 2000, 2999];
-rows_to_plot(5:8) = [3000,6000,9500,14000];
-rows_to_plot(9:24) = [22500:8500:150000];
+% rows_to_plot(1:4) = [1, 1000, 2000, 2999];
+% rows_to_plot(5:8) = [3000,6000,9500,14000];
+rows_to_plot(1:4) = [3000, 8000, 13000, 18000];
+rows_to_plot(5:8) = [28000,38000,48000,58000];
+% rows_to_plot(9:24) = [22500:8500:150000];
 num_plots = length(rows_to_plot);
 
 % Create large enough figure for square subplots
-figure('Units', 'pixels', 'Position', [100, 100, 600, 1500]);
+figure('Units', 'pixels', 'Position', [100, 100, 600, 300]);
 
 % Set up 6x4 layout with tight spacing
-t = tiledlayout(6, 4, 'TileSpacing', 'compact', 'Padding', 'none');
+t = tiledlayout(2, 4, 'TileSpacing', 'compact', 'Padding', 'none');
 
 for idx = 1:num_plots
     i = rows_to_plot(idx);
@@ -26,5 +28,5 @@ for idx = 1:num_plots
 end
 
 % Optional: add a colorbar
-cb = colorbar;
+% cb = colorbar;
 cb.Layout.Tile = 'east';  % place on the right outside the grid
